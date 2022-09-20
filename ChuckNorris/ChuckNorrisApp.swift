d83a3241f065b9c30d12482ea4e82bcacc6831a3
Byron Mejia <isitsmt-4118@ucan.edu.ni>
2022-09-19 21:21:02 -0600

    Initial Commit

---
+++ ChuckNorris/ChuckNorrisApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChuckNorrisApp.swift
+//  ChuckNorris
+//
+//  Created by Byron Mejia on 9/19/22.
+//
+
+import SwiftUI
+
+@main
+struct ChuckNorrisApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
